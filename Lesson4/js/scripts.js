6409e11eace28381d4a11ffee1d0ec8e442e4733
Ltisch Amos <>
2020-05-13 13:19:32 +0200

    Added lesson 3 and 4

--- Lesson4/js/scripts.js
+++ Lesson4/js/scripts.js
@@ -1,1 +1,20 @@
-console.log('WomenWhoCode');
+var urlParams = new URLSearchParams(window.location.search);
+var name = urlParams.get('name') ? urlParams.get('name') : 'not set.';
+console.log(name);
+$('span.name').text(name);
+
+
+
+
+function displayRadioValue() { 
+  var ele = document.getElementsByName('gender'); 
+  
+  for(i = 0; i < ele.length; i++) { 
+  
+    if(ele[i].checked) { 
+    	
+    		document.getElementById("result").innerHTML = "Gender: "+ele[i].value; 
+    } 
+
+  } 
+} 
